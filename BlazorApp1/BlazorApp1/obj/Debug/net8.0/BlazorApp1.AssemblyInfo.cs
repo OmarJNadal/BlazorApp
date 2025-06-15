ac92e229d59d753a57674ecf9e660a4e7e915950
OmarJNadal <omarles4568@gmail.com>
2025-06-14 20:30:28 -0400

    Refactor home,NavMenu,weather,counter,mainlayout

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe240af93267a929c06232e97beae9efd44496e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72dd83cb45510ca04cfeb4cdc156cef09157bdce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
